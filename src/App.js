355cd612e4760f7cf03cea864693b56bf2fbd441
Amr Aly <amr.aly57@yahoo.com>
2022-08-21 01:11:48 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import StripesWrapper from "./components/StripesWrapper";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full">
       <Header />
       <StripesWrapper />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,31 @@
-const Header = () => <header>Header component</header>;
+const Header = () => {
+  return (
+    <header className="bg-cyan-900">
+      <nav className="relative mx-auto flex flex-row flex-wrap items-center justify-center p-4 container w-9/12 text-sky-50">
+        <div className="nav-child">Generate a New Array!</div>
+        <div className="vl"></div>
+        <div className="nav-child flex flex-row flex-wrap items-center justify-between">
+          <div>Change array size</div>
+          <div>
+            <input
+              className="align-middle ml-2"
+              type="range"
+              min="1"
+              max="100"
+              id="myRange"
+            />
+          </div>
+        </div>
+        <div className="vl"></div>
+        <div className="nav-child">Merge Sort</div>
+        <div className="nav-child">Quick Sort</div>
+        <div className="nav-child">Bubble Sort</div>
+        <div className="nav-child">
+          <button>Sort</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
