907cf5664125e18da704c75934763e393a7fe547
Amr Aly <amr.aly57@yahoo.com>
2022-08-25 05:28:47 +0200

    Added skeleton of sort function

--- src/utilities/algorithms.js
+++ src/utilities/algorithms.js
@@ -1,18 +1,49 @@
+const MERGE_SORT = "Merge Sort";
+const QUICK_SORT = "Quick Sort";
+const BUBBLE_SORT = "Bubble Sort";
+
 const sort = (sortingAlgorithm) => {
-  const SORTING_ALGORITHMS = ["Merge Sort", "Quick Sort", "Bubble Sort"];
-
-  if (!SORTING_ALGORITHMS.includes(sortingAlgorithm)) {
+  if (!isValidSortingAlgorithm(sortingAlgorithm)) {
     return;
   }
 
+  if (!stripesAreFound()) {
+    return;
+  }
+
+  switch (sortingAlgorithm) {
+    case MERGE_SORT:
+      mergeSort();
+      return;
+    case QUICK_SORT:
+      quickSort();
+      return;
+    case BUBBLE_SORT:
+      bubbleSort();
+      return;
+  }
+};
+
+const mergeSort = () => {
+  console.log("Merge Sort");
+};
+
+const quickSort = () => {
+  console.log("Quick Sort");
+};
+
+const bubbleSort = () => {
+  console.log("Bubble Sort");
+};
+
+const isValidSortingAlgorithm = (algorithm) => {
+  const SORTING_ALGORITHMS = [MERGE_SORT, QUICK_SORT, BUBBLE_SORT];
+  return SORTING_ALGORITHMS.includes(algorithm);
+};
+
+const stripesAreFound = () => {
   const stripes = document.querySelectorAll(".stripe");
-
-  if (!stripes) {
-    return;
-  }
-
-  console.log(sortingAlgorithm);
-  console.log(stripes);
+  return stripes.length > 0;
 };
 
 export default sort;
